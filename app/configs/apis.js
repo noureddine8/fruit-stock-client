e5aee3087881968a3adf7c13637880c967405003
Louafi Noureddine <noureddinelouafi8@gmail.com>
2021-10-12 10:53:39 +0100

    Set up react navigation

--- app/configs/apis.js
+++ app/configs/apis.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://192.168.1.13:5000';
+const url = 'http://10.1.4.105:5000';
 
 export const getStoreByCity = async city => {
   const data = await axios.get(`${url}/stock/${city}`, {
---
+++ app/utils/navigation/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../../containers/Home';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
