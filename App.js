e6b030785566bfb638407f67a3676a01cf9f5d9d
Louafi Noureddine <noureddinelouafi8@gmail.com>
2021-10-12 12:02:49 +0100

    added the react native picker

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import styles from './app/containers/Home/styles';
+
 import Navigator from './app/utils/navigation';
 
 const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <Navigator />
     </SafeAreaView>
   );
--- app/containers/TransferItem/index.js
+++ app/containers/TransferItem/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, TouchableOpacity, View} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
-function index() {
+function TransferItem() {
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
   return (
     <View style={{padding: 40}}>
-      <Text>HAHAHA</Text>
+      <Picker
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+      <TouchableOpacity onPress={() => console.log(selectedLanguage)}>
+        <Text>Press</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-export default index;
+export default TransferItem;
